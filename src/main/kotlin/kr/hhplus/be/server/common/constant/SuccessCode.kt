7451cded26c979be295d6efad25d7bd80d285df0
yongckim <zxcv9203@naver.com>
2025-01-03 03:06:24 +0900

    :sparkles: feat : 공통 응답 객체 및 코드 정의

---
+++ src/main/kotlin/kr/hhplus/be/server/common/constant/SuccessCode.kt
@@ -0,0 +1,21 @@
+package kr.hhplus.be.server.common.constant
+
+import org.springframework.http.HttpStatus
+
+enum class SuccessCode(
+    val status: HttpStatus,
+    val message: String,
+) {
+    USER_BALANCE_CHARGE(HttpStatus.OK, "잔액 충전에 성공했습니다."),
+    USER_BALANCE_QUERY(HttpStatus.OK, "잔액 조회에 성공했습니다."),
+
+    PRODUCT_QUERY(HttpStatus.OK, "상품 조회에 성공했습니다."),
+    POPULAR_PRODUCT_QUERY(HttpStatus.OK, "인기 상품 조회에 성공했습니다."),
+
+    COUPON_ISSUE_SUCCESS(HttpStatus.CREATED, "쿠폰 발급에 성공했습니다."),
+    COUPON_LIST_QUERY(HttpStatus.OK, "내 쿠폰 목록 조회에 성공했습니다."),
+
+    ORDER_CREATED(HttpStatus.CREATED, "주문이 성공적으로 생성되었습니다."),
+
+    PAYMENT_COMPLETED(HttpStatus.OK, "결제가 성공적으로 완료되었습니다."),
+}
