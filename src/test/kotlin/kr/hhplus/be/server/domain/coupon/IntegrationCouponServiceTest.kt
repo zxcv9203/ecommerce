a52bb4d658f874a56e2aea4b393b1b679300ed4d
yongckim <zxcv9203@naver.com>
2025-01-24 03:03:07 +0900

    :white_check_mark: test : 주문시 쿠폰 예약에 대한 동시성 테스트 추가

--- src/test/kotlin/kr/hhplus/be/server/domain/coupon/IntegrationCouponServiceTest.kt
+++ src/test/kotlin/kr/hhplus/be/server/domain/coupon/IntegrationCouponServiceTest.kt
@@ -1,19 +1,18 @@
 package kr.hhplus.be.server.domain.coupon
 
+import kr.hhplus.be.server.domain.order.Order
 import kr.hhplus.be.server.helper.ConcurrentTestHelper
 import kr.hhplus.be.server.infrastructure.persistence.coupon.DataJpaCouponPolicyRepository
 import kr.hhplus.be.server.infrastructure.persistence.coupon.DataJpaCouponRepository
+import kr.hhplus.be.server.infrastructure.persistence.order.DataJpaOrderRepository
 import kr.hhplus.be.server.infrastructure.persistence.user.DataJpaUserRepository
 import kr.hhplus.be.server.stub.CouponFixture
+import kr.hhplus.be.server.stub.OrderFixture
 import kr.hhplus.be.server.stub.UserFixture
 import kr.hhplus.be.server.template.IntegrationTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.repository.findByIdOrNull
 
 class IntegrationCouponServiceTest : IntegrationTest() {
     @Autowired
@@ -28,36 +27,44 @@ class IntegrationCouponServiceTest : IntegrationTest() {
     @Autowired
     private lateinit var dataJpaCouponRepository: DataJpaCouponRepository
 
+    @Autowired
+    private lateinit var dataJpaOrderRepository: DataJpaOrderRepository
+
+    private lateinit var coupon: Coupon
+
+    private lateinit var order: Order
+
     @BeforeEach
     fun setUp() {
-        val users = (1..11L).map { UserFixture.create(id = 0L, name = "user $it") }
-        dataJpaUserRepository.saveAllAndFlush(users)
+        val user = UserFixture.create(id = 0L, name = "user")
+        dataJpaUserRepository.saveAndFlush(user)
 
         val couponPolicy = CouponFixture.createPolicy(id = 0L, totalCount = 10, currentCount = 0)
         dataJpaCouponPolicyRepository.saveAndFlush(couponPolicy)
+
+        coupon = CouponFixture.create(id = 0L, policy = couponPolicy)
+        dataJpaCouponRepository.saveAndFlush(coupon)
+
+        order = OrderFixture.create(id = 0L, user = user)
+        dataJpaOrderRepository.saveAndFlush(order)
     }
 
     @Nested
-    @DisplayName("쿠폰 발급 동시성 테스트")
-    inner class IssueCouponConcurrencyTest {
+    @DisplayName("쿠폰 예약 동시성 테스트")
+    inner class Reserve {
         @Test
-        @DisplayName("[동시성] 동시에 11명의 사용자가 쿠폰을 발급할 때 10번 성공하고 1번 실패해야 한다.")
-        fun issueCouponConcurrencyTest() {
-            val couponPolicyId = 1L
-
+        @DisplayName("[동시성] 같은 사용자가 5번 주문 건에 대해 쿠폰 예약을 시도할 때 1번만 성공하고 4번 실패한다.")
+        fun reserveCouponConcurrencyTest() {
             val result =
-                ConcurrentTestHelper.executeAsyncTasksWithIndex(11) {
-                    val user = dataJpaUserRepository.findByIdOrNull(it.toLong() + 1)
-                    couponService.issue(user!!, couponPolicyId)
+                ConcurrentTestHelper.executeAsyncTasksWithIndex(5) {
+                    couponService.reserve(coupon = coupon, order = order)
                 }
 
             val successCount = result.count { it }
-            val failCount = result.count { !it }
-            val issuedCoupons = dataJpaCouponRepository.findAll()
+            val failedCount = result.count { !it }
 
-            assertThat(successCount).isEqualTo(10)
-            assertThat(failCount).isEqualTo(1)
-            assertThat(issuedCoupons.size).isEqualTo(successCount)
+            assertThat(successCount).isEqualTo(1)
+            assertThat(failedCount).isEqualTo(4)
         }
     }
 }
